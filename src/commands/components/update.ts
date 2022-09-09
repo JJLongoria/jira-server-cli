f12947e597d42293522580e9d391d9952c081f3f
Juan José Longoria <juanjoselongoria@gmail.com>
2022-09-10 01:26:32 +0200

    Created components:update command

---
+++ src/commands/components/update.ts
@@ -0,0 +1,46 @@
+import { Flags } from "@oclif/core";
+import { Component, JiraServerConnector } from "jira-server-connector";
+import { BaseCommand, BuildFlags } from "../../libs/core/baseCommand";
+import { JiraCLIResponse } from "../../libs/core/jiraResponse";
+import { ComponentColumns } from "../../libs/core/tables";
+import { UX } from "../../libs/core/ux";
+
+export default class Update extends BaseCommand {
+    static description = 'Modify a component via PUT. Any fields present in the PUT will override existing values. As a convenience, if a field is not present, it is silently ignored. Return the update component. ' + UX.processDocumentation('<doc:Component>');
+    static examples = [
+        `$ jiraserver components:update -a "MyAlias" --component "theComponentId" --data "{'name':'Component 1','description':'This is a Jira component','leadUserName':'fred','assigneeType':'PROJECT_LEAD','isAssigneeTypeValid':false,'project':'PROJECTKEY','projectId':10000}"  --json`,
+        `$ jiraserver components:update -a "MyAlias" --component "theComponentId" --file "path/to/the/json/data/file" --csv`,
+        `$ jiraserver components:update -a "MyAlias" --component "theComponentId" --file "path/to/the/json/data/file"`,
+    ];
+    static flags = {
+        ...BaseCommand.flags,
+        alias: BuildFlags.alias,
+        csv: BuildFlags.csv,
+        extended: BuildFlags.extended,
+        data: BuildFlags.input.jsonData('<doc:ComponentInput>'),
+        file: BuildFlags.input.jsonFile('<doc:ComponentInput>'),
+        component: Flags.string({
+            description: 'The Component Id to update',
+            required: true,
+            name: 'Component Id',
+        }),
+    };
+    async run(): Promise<JiraCLIResponse<Component>> {
+        const response = new JiraCLIResponse<Component>();
+        const connector = new JiraServerConnector(this.localConfig.getConnectorOptions(this.flags.alias));
+        try {
+            const result = await connector.components.create(this.getJSONInputData());
+            response.result = result
+            response.status = 0;
+            response.message = this.getRecordUpdatedText('Component');
+            this.ux.log(response.message);
+            this.ux.table<Component>([result], ComponentColumns, {
+                csv: this.flags.csv,
+                extended: this.flags.extended,
+            });
+        } catch (error) {
+            this.processError(response, error);
+        }
+        return response;
+    }
+}
