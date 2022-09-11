9a8b36afd003b70ec5cd83dc71bc4b62e08b0cc2
Juan José Longoria <juanjoselongoria@gmail.com>
2022-09-11 22:17:47 +0200

    Created issues:watchers:add command

---
+++ src/commands/issues/watchers/add.ts
@@ -0,0 +1,40 @@
+import { Flags } from "@oclif/core";
+import { JiraServerConnector } from "jira-server-connector";
+import { BaseCommand, BuildFlags } from "../../../libs/core/baseCommand";
+import { JiraCLIResponse } from "../../../libs/core/jiraResponse";
+
+export default class Watch extends BaseCommand {
+    static description = 'Adds a user to an issue\'s watcher list.';
+    static examples = [
+        `$ jiraserver issues:votes:vote -a "MyAlias" --issue "theIssueKeyOrId" --user "theUsername" --json`,
+        `$ jiraserver issues:votes:vote -a "MyAlias" --issue "theIssueKeyOrId" --user "theUsername" --csv`,
+        `$ jiraserver issues:votes:vote -a "MyAlias" --issue "theIssueKeyOrId" --user "theUsername"`,
+    ];
+    static flags = {
+        ...BaseCommand.flags,
+        alias: BuildFlags.alias,
+        issue: Flags.string({
+            description: 'The Issue key or id to watch',
+            required: true,
+            name: 'Issue Key or Id',
+        }),
+        user: Flags.string({
+            description: 'The Username to watch the issue',
+            required: true,
+            name: 'User',
+        }),
+    };
+    async run(): Promise<JiraCLIResponse<any>> {
+        const response = new JiraCLIResponse<any>();
+        const connector = new JiraServerConnector(this.localConfig.getConnectorOptions(this.flags.alias));
+        try {
+            const result = await connector.issues.watchers(this.flags.issue).add(this.flags.user);
+            response.status = 0;
+            response.message = 'Issue watched successffully';
+            this.ux.log(response.message);
+        } catch (error) {
+            this.processError(response, error);
+        }
+        return response;
+    }
+}
