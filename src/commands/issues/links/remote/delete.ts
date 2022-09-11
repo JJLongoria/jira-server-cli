4be26f611ef4fd61d64fa7bbbec85b8fe76f9158
Juan José Longoria <juanjoselongoria@gmail.com>
2022-09-11 21:01:46 +0200

    Created issues:links:remote:delete command

---
+++ src/commands/issues/links/remote/delete.ts
@@ -0,0 +1,41 @@
+import { Flags } from "@oclif/core";
+import { JiraServerConnector } from "jira-server-connector";
+import { BaseCommand, BuildFlags } from "../../../../libs/core/baseCommand";
+import { JiraCLIResponse } from "../../../../libs/core/jiraResponse";
+import { UX } from "../../../../libs/core/ux";
+
+export default class Upsert extends BaseCommand {
+    static description = 'Delete the remote issue link with the given global id on the issue.';
+    static examples = [
+        `$ jiraserver issues:links:remote:delete -a "MyAlias" --issue "theIssueKeyOrId" --link "theLinkGlobalId" --json`,
+        `$ jiraserver issues:links:remote:delete -a "MyAlias" --issue "theIssueKeyOrId" --link "theLinkGlobalId" --csv`,
+        `$ jiraserver issues:links:remote:delete -a "MyAlias" --issue "theIssueKeyOrId" --link "theLinkGlobalId"`,
+    ];
+    static flags = {
+        ...BaseCommand.flags,
+        alias: BuildFlags.alias,
+        issue: Flags.string({
+            description: 'The Issue key or id to upsert remote link',
+            required: true,
+            name: 'Issue Key or Id',
+        }),
+        link: Flags.string({
+            description: 'The Link Global Id to delete',
+            required: true,
+            name: 'Link Global Id',
+        }),
+    };
+    async run(): Promise<JiraCLIResponse<any>> {
+        const response = new JiraCLIResponse<any>();
+        const connector = new JiraServerConnector(this.localConfig.getConnectorOptions(this.flags.alias));
+        try {
+            const result = await connector.issues.remoteLinks(this.flags.issue).delete(this.flags.link);
+            response.status = 0;
+            response.message = this.getRecordDeletedText('Issue Remote Link');
+            this.ux.log(response.message);
+        } catch (error) {
+            this.processError(response, error);
+        }
+        return response;
+    }
+}
