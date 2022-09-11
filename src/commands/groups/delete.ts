d945e1e220070d3371017625c50fcdf9766e3d08
Juan José Longoria <juanjoselongoria@gmail.com>
2022-09-11 14:08:40 +0200

    Created groups:delete command

--- src/commands/groups/delete.ts
+++ src/commands/groups/delete.ts
@@ -1,39 +1,38 @@
 import { Flags } from "@oclif/core";
-import { Group, JiraServerConnector } from "jira-server-connector";
+import { JiraServerConnector } from "jira-server-connector";
 import { BaseCommand, BuildFlags } from "../../libs/core/baseCommand";
 import { JiraCLIResponse } from "../../libs/core/jiraResponse";
-import { GroupColumns } from "../../libs/core/tables";
-import { UX } from "../../libs/core/ux";
 
 export default class Create extends BaseCommand {
-    static description = 'Creates a group by given group parameter. ' + UX.processDocumentation('<doc:Group>');
+    static description = 'Deletes a group by given group parameter. ';
     static examples = [
         `$ jiraserver groups:create -a "MyAlias" --name "theGroupName" --json`,
-        `$ jiraserver groups:create -a "MyAlias" --name "theGroupName" --csv`,
+        `$ jiraserver groups:create -a "MyAlias" --name "theGroupName" --swap "anotherGroupName" --csv`,
         `$ jiraserver groups:create -a "MyAlias" --name "theGroupName"`,
     ];
     static flags = {
         ...BaseCommand.flags,
         alias: BuildFlags.alias,
-        csv: BuildFlags.csv,
         name: Flags.string({
-            description: 'The group name to create',
+            description: 'The group name to delete',
             required: true,
             name: 'Name',
         }),
+        swap: Flags.string({
+            description: 'If you delete a group and content is restricted to that group, the content will be hidden from all users. To prevent this, use this parameter to specify a different group to transfer the restrictions (comments and worklogs only) to.',
+            required: false,
+            name: 'Swap',
+        }),
     };
-    async run(): Promise<JiraCLIResponse<Group>> {
-        const response = new JiraCLIResponse<Group>();
+    async run(): Promise<JiraCLIResponse<any>> {
+        const response = new JiraCLIResponse<any>();
         const connector = new JiraServerConnector(this.localConfig.getConnectorOptions(this.flags.alias));
         try {
-            const result = await connector.groups.create(this.flags.name);
+            const result = await connector.groups.delete(this.flags.name, this.flags.swap);
             response.result = result
             response.status = 0;
-            response.message = this.getRecordCreatedText('Group');
+            response.message = this.getRecordDeletedText('Group');
             this.ux.log(response.message);
-            this.ux.table<Group>([result], GroupColumns, {
-                csv: this.flags.csv,
-            });
         } catch (error) {
             this.processError(response, error);
         }
